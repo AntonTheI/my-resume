63c547286b3791ac9307f034227b0832d731f57d
Anton I <anton333mai@gmail.com>
2025-12-31 17:02:44 +0900

    add close tab btn

--- src/components/Layout/AppBar/WindowControl.tsx
+++ src/components/Layout/AppBar/WindowControl.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import WindowControlButton from "../../ui/WindowControlButton";
 
 const WindowControl = () => {
--- src/components/Layout/editor/FileTab.tsx
+++ src/components/Layout/editor/FileTab.tsx
@@ -1,10 +1,16 @@
 const FileTab = ({ tabs }: { tabs: string[] }) => {
   return (
-    <div className="flex flex-row ">
+    <div className="flex flex-row cursor-pointer  ">
       {tabs.map((name) => {
         return (
-          <div className="px-2 border-black border" key={name}>
+          <div
+            className="flex items-center px-2 border-black border group"
+            key={name}
+          >
             {name}
+            <div className="ml-1 px-1 items-center opacity-0 group-hover:opacity-100 hover:bg-white/10 ">
+              x
+            </div>
           </div>
         );
       })}
